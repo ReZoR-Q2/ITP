5536d7e12e38c1754d0413e7a0fd6a327a8bc436
ReZoR-Q2 <rezorgerman@gmail.com>
2025-05-11 19:33:49 +0300

    djindga

---
+++ ITP/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+
+# def about(request):
+#     return HttpResponse("<h4> Страница про нас </h4>")
