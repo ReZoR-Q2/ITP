978c2bc42205f40142de4a33e607480f4753eedc
ReZoR-Q2 <rezorgerman@gmail.com>
2025-05-13 00:07:23 +0300

    fix views.news_home

--- ITP/news/urls.py
+++ ITP/news/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.new_home, name='new_home'),
+    path('', views.news_home, name='news_home')
 ]
--- ITP/news/views.py
+++ ITP/news/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def new_home(request):
+def news_home(request):
     return render(request, 'news/news_home.html')
